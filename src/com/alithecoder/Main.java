9d21c2c58ec6519fa912189aef4d0447b7f3ad41
Ali Hasan <capt.3lii@gmail.com>
2025-07-26 16:14:16 +0300

    test code

--- src/com/alithecoder/Main.java
+++ src/com/alithecoder/Main.java
@@ -2,6 +2,10 @@ package com.alithecoder;
 
 public class Main {
     public static void main(String[] args) {
-
+        String name = "ali";
+        int alisAge = 29;
+        if (name == "ali" && alisAge >21 ){
+            System.out.println("Ignore ali");
+        }
     }
 }
